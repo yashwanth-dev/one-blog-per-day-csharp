d91932771e2c15b023899734c9da7762d0e731f6
yashwanth-dev <yashwanthp.developer@github.com>
2023-05-09 21:38:57 +0530

    initial commit

---
+++ src/AsynchronousCSharp/Cook.cs
@@ -0,0 +1,46 @@
+﻿namespace AsynchronousCSharp;
+
+// https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/
+
+public class Cook
+{
+    public Cook()
+    {
+        PourCoffee();
+        FryEggs();
+        FryBacon();
+        ToastBread();
+        AddButterAndJam();
+        PourOrangeJuice();
+    }
+    
+    public void PourCoffee()
+    {
+        Console.WriteLine("Pour Coffee!!");
+    }
+
+    public void FryEggs()
+    {
+        Console.WriteLine("Fried Eggs!!");
+    }
+
+    public void FryBacon()
+    {
+        Console.WriteLine("Fried Bacon!!");
+    }
+
+    public void ToastBread()
+    {
+        Console.WriteLine("Toasted bread!!");
+    }
+
+    public void AddButterAndJam()
+    {
+        Console.WriteLine("Added butter and Jam!!");
+    }
+
+    public void PourOrangeJuice()
+    {
+        Console.WriteLine("Pour Orange Juice");
+    }
+}
---
+++ test/AsynchronousCSharp.Tests/CookTests.cs
@@ -0,0 +1,10 @@
+namespace AsynchronousCSharp.Tests;
+
+public class CookTests
+{
+    [Fact]
+    public void MustCook()
+    {
+        var cook = new Cook();
+    }
+}
